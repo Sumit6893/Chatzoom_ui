39811071ef0bd0844132c34c90c7019b4622efa5
Sumit6893 <abhisonu6893@gmail.com>
2026-05-03 09:57:51 +0530

    header_navigation_change

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Page/Home";
 import Login from "./Page/Login";
 import Register from "./Page/Resister";
+import Features from "./Components/Features";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/features" element={Features} />
       </Routes>
     </BrowserRouter>
   );
