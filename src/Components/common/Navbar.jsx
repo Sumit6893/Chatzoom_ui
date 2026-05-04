214cd01114360a4a4bd305d7a85f9e3bf2da5980
Sumit6893 <abhisonu6893@gmail.com>
2026-05-04 23:50:14 +0530

    Routes are created

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
+      setScrolled(window.scrollY > 80);
     };
 
     window.addEventListener("scroll", handleScroll);
